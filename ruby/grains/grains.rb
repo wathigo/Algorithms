ac455bbddbb2288fb90751c6e6558b278d5d2f26
wathigo <wathigosimon@gmail.com>
2021-01-05 07:49:48 -0500

    Solve the grains challenge

---
+++ ruby/grains/grains.rb
@@ -0,0 +1,27 @@
+class Grains 
+    def self.square num
+        raise ArgumentError.new("Invalid Argument") if num <= 0 || num > 64
+        no_of_grains = self.calculate num 
+    end
+
+    def self.total
+        total = 0
+        (1..64).each do |square|
+            total += self.calculate square
+        end
+        total
+    end
+
+    def self.calculate num
+        counter = 1
+        grains = 1
+        while counter < num
+            # print(counter, " ", grains)
+            grains *= 2
+            counter += 1
+        end 
+        grains
+    end
+end
+
+puts Grains.square(16)
